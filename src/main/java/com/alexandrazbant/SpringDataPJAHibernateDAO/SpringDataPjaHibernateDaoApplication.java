84b7ab99063b5713da6d86d2705edc2021a7472f
AlexandraZbant <alexandrazbant@gmail.com>
2023-01-24 16:48:53 +0200

    first commit

---
+++ src/main/java/com/alexandrazbant/SpringDataPJAHibernateDAO/SpringDataPjaHibernateDaoApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrazbant.SpringDataPJAHibernateDAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataPjaHibernateDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataPjaHibernateDaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandrazbant/SpringDataPJAHibernateDAO/SpringDataPjaHibernateDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrazbant.SpringDataPJAHibernateDAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataPjaHibernateDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
